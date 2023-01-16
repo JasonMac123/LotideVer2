3ed0ec62c4b4d4f9b8779508590837b880a4f60d
JasonSnow123 <m.jason.2018@gmail.com>
2023-01-16 11:03:51 -0500

    removed unused code

--- map.js
+++ map.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertEqualArrays = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -26,3 +6,4 @@ const map = function(array, callback) {
   return results;
 };
 
+module.exports = map;
